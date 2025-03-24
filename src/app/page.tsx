611323181dfd3759506ac4f488c17518816d9a4b
Jacob Schwartz <schwja04@gmail.com>
2025-03-24 09:17:33 -0500

    Don't show gallery unless logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
+
+async function Images() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + '-' + index} className="w-48">
+          <img src={image.url} />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default async function HomePage() {
   const images = await db.query.images.findMany({
@@ -10,13 +27,12 @@ export default async function HomePage() {
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} />
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl">Please sign in</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
